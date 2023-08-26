773d8dcd052876622e093ff912b30ab94c8201a1
xhvsh <82961902+xhvsh@users.noreply.github.com>
2023-08-26 15:12:28 +0200

    Add files

---
+++ script.js
@@ -0,0 +1,44 @@
+const h1 = "Searching location...",
+  h2 = "Analyzing the clouds... ",
+  h3 = "Looking outside your window... ",
+  h4 = "Gathering last information... ",
+  h5 = "Idk, just look outside.";
+
+const input = document.querySelector(".input"),
+  btn = document.querySelector("button"),
+  progress = document.querySelector(".progress-container"),
+  title = document.querySelector("h1");
+
+btn.addEventListener("click", () => {
+  input.classList.add("hidden");
+  progress.classList.remove("hidden");
+
+  title.innerHTML = h1;
+
+  const progressbar = document.querySelector(".progress");
+
+  const changeProgress = (progress) => {
+    progressbar.style.width = `${progress}%`;
+  };
+
+  setTimeout(() => {
+    changeProgress(22);
+    title.innerHTML = h1;
+  }, 3000);
+  setTimeout(() => {
+    changeProgress(45);
+    title.innerHTML = h2;
+  }, 3500);
+  setTimeout(() => {
+    changeProgress(85);
+    title.innerHTML = h3;
+  }, 4600);
+  setTimeout(() => {
+    changeProgress(98);
+    title.innerHTML = h4;
+  }, 5266);
+  setTimeout(() => {
+    changeProgress(100);
+    title.innerHTML = h5;
+  }, 8000);
+});
